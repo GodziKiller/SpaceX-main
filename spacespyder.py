e2146a2c71193b0c7a9a4e42ad5d717bd5e1aede
Thurn4X <113912459+Thurn4X@users.noreply.github.com>
2022-12-05 17:49:50 +0100

    Add files via upload

--- spacespyder.py
+++ spacespyder.py
@@ -9,13 +9,24 @@ Code pour faire un space invader
 
 
 from tkinter import *
+import threading
+import time
 
-kaarisx=0
+kaarisx=650
+boobax=50
+boobay=200
+boobadx=10
+y=0
+print(boobax)
 
 Mafenetre=Tk()
 #Mafenetre.attributes('-fullscreen', True)
 background= PhotoImage(file = 'background.gif')
 kaaris=PhotoImage(file= 'kaarisvaisseau.png')
+booba=PhotoImage(file= 'booba.png')
+cd=PhotoImage(file= 'cd.png')
+
+
 Mafenetre.geometry('1500x1000+400+200')
 
 
@@ -28,10 +39,65 @@ Canevas= Canvas(Framebase,width=1500,height=1000)
 Canevas.pack(fill=BOTH, expand=True)
 
 Canevas.create_image(0,0,anchor=NW, image=background)
+kaarisimage=Canevas.create_image(kaarisx,850,image=kaaris)
+boobaimage=Canevas.create_image(boobax,200,image=booba)
+# Canevaskaaris=Canvas(Canevas,width=100,height=100)
+# #Canevaskaaris.pack(side=BOTTOM,pady=10)
+# Canevaskaaris.place(x=kaarisx,y=850)
 
-Canevaskaaris=Canvas(Canevas,width=100,height=100)
-Canevaskaaris.pack(side=BOTTOM,pady=10)
-Canevaskaaris.create_image(0,0,anchor=NW, image=kaaris)
+# Canevaskaaris.create_image(0,0,anchor=NW, image=kaaris)
+
+
+
+def deplacerbooba(boobadx):
+    global boobax
+    while True:
+        
+        boobax+=boobadx
+        if boobax+50>=1500:
+            boobadx=-boobadx
+        if boobax-50<=0:
+            boobadx=-boobadx
+            
+        
+        Canevas.move(boobaimage,boobadx,0)
+        time.sleep(0.05)
+
+
+t1=threading.Thread(target=lambda : deplacerbooba(boobadx))
+t1.start()
+t1=threading.Thread(target=lambda : tir(boobadx))
+t1.start()
+
+def left(event):
+    global kaarisx
+    if kaarisx-50<=0:
+        return
+    else:
+            
+        x = -10
+        kaarisx+=x
+        Canevas.move(kaarisimage,x,y)
+    
+def right(event):
+    global kaarisx
+    if kaarisx+50>=1500:
+        return
+    else:
+            
+        x = 10
+        kaarisx+=x
+        Canevas.move(kaarisimage,x,y)
+        
+# def tir(event):
+#     global kaarisx
+#     cd=Canevas.create_image(kaarisx,850,image=cd)
+
+
+Mafenetre.bind("<Left>",left)
+Mafenetre.bind("<Right>",right)
+# Mafenetre.bind("<space>",tir)
+
 
 
 
